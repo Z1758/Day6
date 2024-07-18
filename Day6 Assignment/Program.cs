a3c995917df8dc89c6e73b17e6845188ab5b55be
Z1758 <timeofwitch@gmail.com>
2024-07-18 16:55:08 +0900

    Initial commit

---
+++ Day6 Assignment/Program.cs
@@ -0,0 +1,109 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Day6_Assignment
+{
+    internal class Program
+    {
+
+
+  
+
+        static public int Assignment1(int num)
+        {
+            int sum = 0;
+            for (int i = 0; i <= num; i++) {
+
+                if (i % 3 == 0)
+                {
+                    sum += i;
+                }
+                else if (i % 5 == 0)
+                {
+                    sum += i;
+
+                }
+            }
+
+            return sum;
+        }
+      
+        static public int Assignment2(int num)
+        {
+            int sum = 0;
+
+            string str = num.ToString();
+
+            for(int i = 0;i<str.Length; i++ )
+            {
+                //문자열을 배열로 int.Parse 할때 다시 문자열로 형변환 해줘야 한다
+                sum+=int.Parse(str[i].ToString());
+            }
+            return sum;
+        }
+
+        //반복문 피보나치
+        static public int Assignment3_Fibonacci(int num)
+        {
+            if (num == 1||num == 2)
+            {
+                return 1;
+            }
+
+            int x = 1;
+            int y = 1;
+            int sum = 0;
+
+            for (int i = 0; i < num - 2; i++)
+            {
+                sum = x + y;
+                x = y;
+                y = sum;      
+            }
+
+
+            return sum;
+        }
+
+        //일반재귀 피보나치
+        static int Fibonacci(int num)
+        {
+            if (num <= 1)
+            {
+                return num;
+            }
+
+            return Fibonacci(num - 1) + Fibonacci(num - 2);
+        }
+
+
+        //꼬리재귀 피보나치
+        //매개변수 num이 반복문의 i 
+        static int FibonacciTail(int num, int x , int y )
+        {
+            if (num == 1)
+                return x;
+            if (num == 2)
+                return y;
+
+            int sum = x + y;
+            x = y;
+            y = sum;
+
+            return FibonacciTail(num - 1, x, y);
+        }
+
+
+   
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("심화 1 " + Assignment1(15));
+            Console.WriteLine("심화 2 " + Assignment2(5611));
+            Console.WriteLine("심화 4 " + Assignment3_Fibonacci(11));
+
+            Console.WriteLine("재귀 " + Fibonacci(11));
+            Console.WriteLine("꼬리재귀 "+ FibonacciTail(11, 1,1));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,180 @@
+﻿namespace Day6
+{
+    internal class Program
+    {
+        /* 과제
+        //배열 내림차순 정렬하는 함수
+        static public float[] DescendingSort(float[] f)
+        {
+            float temp = 0.0f;
+
+            //버블 정렬
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f.Length - 1; j++)
+                {
+                    if (f[j] < f[j + 1])
+                    {
+                        temp = f[j];
+                        f[j] = f[j + 1];
+                        f[j + 1] = temp;
+                    }
+                }
+            }
+            return f;
+
+        }
+
+        //함수 오버라이딩
+        static public int[] DescendingSort(int[] f)
+        {
+            int temp = 0;
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f.Length - 1; j++)
+                {
+                    if (f[j] < f[j + 1])
+                    {
+                        temp = f[j];
+                        f[j] = f[j + 1];
+                        f[j + 1] = temp;
+
+                    }
+
+                }
+            }
+            return f;
+
+        }
+
+
+        static public int Assignment1(int a, int b, int c, int d)
+        {
+            int[] ints = { a, b, c, d };
+
+
+            DescendingSort(ints);
+
+            return ints[0];
+
+        }
+
+        static public float Assignment2(float a1, float a2, float a3, float a4, float a5)
+        {
+            float[] floats = { a1,a2,a3,a4,a5 };
+
+            floats = DescendingSort(floats);
+
+            return floats[0] + floats[1];
+        }
+
+
+        static public bool Assignment3(int a, int b)
+        {
+            bool flag=true;
+            if (a > b) {
+                if (a <= b + 99)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            else
+            {
+                if (a +99 >= b)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
+            
+            }
+
+            return flag;
+
+        }
+        */
+        enum Direction { Up, Down, Left, Right }
+
+        public struct Monster
+        {
+            public string name;
+            public int attack;
+
+        }
+        public static Monster MakeMonster(string mob)
+        {
+
+            Monster monster = new Monster();
+            switch (mob)
+            {
+                
+
+                case "오크":
+                    monster.attack = 10;
+                    monster.name = "오크";
+                    break;
+                case "슬라임":
+                    monster.attack = 10;
+                    monster.name = "슬라임";
+                    break;
+                default:
+                    
+                    break;
+            }
+
+            return monster;
+
+        }
+
+        static void Damage(ref int damage)
+        {
+            damage = damage + 10;
+        }
+
+        static void Main(string[] args)
+        {
+            /* 과제
+            Console.WriteLine("과제1 " +Assignment1(3, 15, 8, 10));
+            Console.WriteLine("과제2 " +Assignment2(3.0f, 15.7f, 8.0f, 10.0f, 1.0f));
+            Console.WriteLine("과제3 " +Assignment3(103, 15));
+            */
+            
+            MakeMonster("오크");
+
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+          
+            switch (key)
+            {
+                case ConsoleKey.A:
+                case ConsoleKey.LeftArrow:
+                    Console.WriteLine("좌");
+                    break;
+                case ConsoleKey.D:
+                case ConsoleKey.RightArrow:
+                    Console.WriteLine("우");
+                    break;
+                case ConsoleKey.W:
+                case ConsoleKey.UpArrow:
+                    Console.WriteLine("상");
+                    break;
+                case ConsoleKey.S:
+                case ConsoleKey.DownArrow:
+                    Console.WriteLine("하");
+                    break;
+            }
+
+            int damage = 10;
+            Damage(ref damage);
+            Console.WriteLine(damage);
+
+
+        }
+    }
+}
